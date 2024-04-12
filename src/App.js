59fd04471ca3c97884b30d64bca322543e064021
subitha satheesh <147406447+subitha57@users.noreply.github.com>
2024-04-12 10:03:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home.jsx';
+import Cart from './pages/cart/Cart.jsx';
+import ExploreMenu from './components/ExploreMenu/ExploreMenu.jsx';
+import Footer from './components/footer/Footer.jsx';
+import LoginPopup from './components/login/LoginPopup.jsx';
+import PlaceOrder from './pages/placeOrder/PlaceOrder.jsx'
 
 function App() {
+ 
+const [showLogin,setShowLogin]=useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
+    <div className="app">
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/PlaceOrder' element={<PlaceOrder />}/>
+      </Routes>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css';
+import {assets} from '../../assets/assets';
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className='footer-content'>
+            <div className="footer-content-left">
+                <h1>Tandoori pizza</h1>
+               
+                <p>text</p>
+             <div className='footer-social-icon'>
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+
+             </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+91 00000 00000</li>
+                    <li>contact@dantooripizza.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+        <p className='footer-copyright'>Copyright 2024 @ Tandoori.com - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
